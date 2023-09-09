9ab22b66f5ab60f3ae3038ab3de2c4c0e6b788d5
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-09-08 21:54:32 -0300

    feat: added modelMapper

---
+++ src/main/java/com/finances/common/modelmapper/ModelMapperConfig.java
@@ -0,0 +1,16 @@
+package com.finances.common.modelmapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+        return modelMapper;
+    }
+}
