e5e0344a94f19c0add6ad423ad2156e377110435
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-12-01 12:44:28 -0300

    feat: added cors config

---
+++ src/main/java/com/finances/common/secutiry/resourceserver/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.finances.common.secutiry.resourceserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
